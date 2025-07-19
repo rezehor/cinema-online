4ba00984569a26c130531fa54cd32a2a35e186bf
Timur Khvat <timurkhvat@gmail.com>
2025-07-19 13:55:53 +0200

    Add UserGroup model with Enum-based user roles

--- Cinema/models/users.py
+++ Cinema/models/users.py
@@ -1,5 +1,10 @@
 import enum
 
+from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy.orm import relationship
+
+from Cinema.database import Base
+
 
 class UserGroupEnum(str, enum.Enum):
     USER = "user"
@@ -10,3 +15,11 @@ class UserGroupEnum(str, enum.Enum):
 class GenderEnum(str, enum.Enum):
     MAN = "man"
     WOMAN = "woman"
+
+
+class UserGroup(Base):
+    __tablename__ = "usergroup"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Enum(UserGroupEnum), nullable=False, unique=True)
+    users: list["User"] = relationship("User", back_populates="group")
