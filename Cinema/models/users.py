d149de9902b0dee43c24d1b2c0c1004f1f9f9c6e
Timur Khvat <timurkhvat@gmail.com>
2025-07-19 14:51:06 +0200

    Add User model with relations to group, profile and tokens

--- Cinema/models/users.py
+++ Cinema/models/users.py
@@ -1,6 +1,6 @@
 import enum
 
-from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy import Column, Integer, String, Enum, Boolean, DateTime, func, ForeignKey
 from sqlalchemy.orm import relationship
 
 from Cinema.database import Base
@@ -22,4 +22,32 @@ class UserGroup(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(Enum(UserGroupEnum), nullable=False, unique=True)
+
     users: list["User"] = relationship("User", back_populates="group")
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False, unique=True)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, nullable=False, default=False)
+    created_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now()
+    )
+    updated_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now()
+    )
+    group_id = Column(Integer, ForeignKey("usergroup.id", ondelete="CASCADE"), nullable=False)
+
+    group = relationship("UserGroup", back_populates="user")
+    profile = relationship("UserProfile", back_populates="user", cascade="all, delete-orphan")
+    activation_token = relationship("ActivationToken", back_populates="user", cascade="all, delete-orphan")
+    password_reset_token = relationship("PasswordResetToken", back_populates="user", cascade="all, delete-orphan")
+    refresh_token = relationship("RefreshToken", back_populates="user", cascade="all, delete-orphan")
