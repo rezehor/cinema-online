62bd288ed508cf2b95cb47147135ce15e7516004
Timur Khvat <timurkhvat@gmail.com>
2025-07-20 11:07:16 +0200

    Add Genre model with many-to-many relationship to Movie

--- Cinema/models/movies.py
+++ Cinema/models/movies.py
@@ -15,3 +15,16 @@ MoviesGenres = Table(
         ForeignKey("genres.id", ondelete="CASCADE"),
         primary_key=True, nullable=False),
 )
+
+
+class Genre(Base):
+    __tablename__ = "genres"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+
+    movies = relationship(
+        "Movie",
+        secondary=MoviesGenres,
+        back_populates="genres",
+    )
