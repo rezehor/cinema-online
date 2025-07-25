63a3e6fdbe48342b3d47ae329a7e5e2886607a2c
Timur Khvat <timurkhvat@gmail.com>
2025-07-25 09:42:17 +0200

    Centralize path and template name configurations

--- Cinema/config/settings.py
+++ Cinema/config/settings.py
@@ -1,12 +1,29 @@
 from pathlib import Path
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings):
+class BaseAppSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
+    BASE_DIR: Path = Path(__file__).parent.parent
+    PATH_TO_EMAIL_TEMPLATES_DIR: str = str(BASE_DIR / "notifications" / "templates")
+    ACTIVATION_EMAIL_TEMPLATE_NAME: str = "activation_request.html"
+    ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME: str = "activation_complete.html"
+    PASSWORD_RESET_TEMPLATE_NAME: str = "password_reset_request.html"
+    PASSWORD_RESET_COMPLETE_TEMPLATE_NAME: str = "password_reset_complete.html"
+
     LOGIN_TIME_DAYS: int = 7
+
+    EMAIL_HOST: str = "localhost"
+    EMAIL_PORT: int = 25
+    EMAIL_HOST_USER: str
+    EMAIL_HOST_PASSWORD: str
+    EMAIL_USE_TLS: bool = False
+    MAILHOG_API_PORT: int = 8025
+
+
+class Settings(BaseAppSettings):
+
     SECRET_KEY_ACCESS: str
     SECRET_KEY_REFRESH: str
     JWT_SIGNING_ALGORITHM: str
-
-    class Config:
-        env_file = str(Path(__file__).parent.parent.parent / ".env")
