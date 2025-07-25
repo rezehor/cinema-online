69230b3f395f95cce01b407896f6c61d2f587dc4
Timur Khvat <timurkhvat@gmail.com>
2025-07-25 10:02:33 +0200

    Create dependency provider for email service

--- Cinema/config/dependencies.py
+++ Cinema/config/dependencies.py
@@ -1,5 +1,7 @@
 from fastapi import Depends
-from config.settings import Settings
+from config.settings import Settings, BaseAppSettings
+from notifications.emails import EmailSender
+from notifications.interfaces import EmailSenderInterface
 from security.interfaces import JWTAuthManagerInterface
 from security.token_manager import JWTAuthManager
 
@@ -15,4 +17,21 @@ def get_jwt_auth_manager(
         secret_key_access=settings.SECRET_KEY_ACCESS,
         secret_key_refresh=settings.SECRET_KEY_REFRESH,
         algorithm=settings.JWT_SIGNING_ALGORITHM,
+    )
+
+def get_accounts_email_notificator(
+    settings: BaseAppSettings = Depends(get_settings)
+) -> EmailSenderInterface:
+
+    return EmailSender(
+        hostname=settings.EMAIL_HOST,
+        port=settings.EMAIL_PORT,
+        email=settings.EMAIL_HOST_USER,
+        password=settings.EMAIL_HOST_PASSWORD,
+        use_tls=settings.EMAIL_USE_TLS,
+        template_dir=settings.PATH_TO_EMAIL_TEMPLATES_DIR,
+        activation_email_template_name=settings.ACTIVATION_EMAIL_TEMPLATE_NAME,
+        activation_complete_email_template_name=settings.ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME,
+        password_email_template_name=settings.PASSWORD_RESET_TEMPLATE_NAME,
+        password_complete_email_template_name=settings.PASSWORD_RESET_COMPLETE_TEMPLATE_NAME
     )
