7f239318e6bb3276eb2d073538d301e99394b97d
Timur Khvat <timurkhvat@gmail.com>
2025-07-25 09:43:54 +0200

    Implement password reset confirmation email

--- Cinema/notifications/templates/emails.py
+++ Cinema/notifications/templates/emails.py
@@ -102,3 +102,16 @@ class EmailSender(EmailSenderInterface):
         html_content = template.render(email=email, reset_link=reset_link)
         subject = "Password Reset Request"
         await self._send_email(email, subject, html_content)
+
+    async def send_password_reset_complete_email(self, email: str, login_link: str) -> None:
+        """
+        Send a password reset completion email asynchronously.
+
+        Args:
+            email (str): The recipient's email address.
+            login_link (str): The login link to be included in the email.
+        """
+        template = self._env.get_template(self._password_complete_email_template_name)
+        html_content = template.render(email=email, login_link=login_link)
+        subject = "Your Password Has Been Successfully Reset"
+        await self._send_email(email, subject, html_content)
