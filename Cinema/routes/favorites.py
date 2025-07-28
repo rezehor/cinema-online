4abbe7266297034f9fff42e26251d1c4034040bf
Timur Khvat <timurkhvat@gmail.com>
2025-07-28 13:36:11 +0200

    Add endpoint to add a movie to favorites

---
+++ Cinema/routes/favorites.py
@@ -0,0 +1,54 @@
+from typing import Optional
+
+from fastapi import APIRouter, Depends, HTTPException, status, Query
+from sqlalchemy import select, or_, func
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+
+from Cinema.config.dependencies import get_current_user
+from Cinema.database import get_db
+from Cinema.models import User, Movie, Star, Director, Genre
+from Cinema.models.users import UserFavoriteMovie
+from Cinema.routes.movies import MovieSortByEnum, SortOrderEnum
+from Cinema.schemas.movies import MovieListResponseSchema, MovieListItemSchema
+from Cinema.schemas.users import MessageResponseSchema
+
+router = APIRouter()
+
+
+@router.post(
+    "/{movie_id}",
+    summary="Add a movie to favorites",
+    status_code=status.HTTP_200_OK,
+    response_model=MessageResponseSchema
+)
+async def add_movie_to_favorites(
+        movie_id: int,
+        db: AsyncSession = Depends(get_db),
+        current_user: User = Depends(get_current_user)
+) -> MessageResponseSchema:
+    movie = (await db.execute(select(Movie).where(Movie.id == movie_id))).scalars().first()
+    if not movie:
+        raise HTTPException(status_code=404, detail="Movie not found.")
+
+    if movie in current_user.favorite_movies:
+        raise HTTPException(
+            status_code=400,
+            detail=f"{movie.name} is already in your favorites."
+        )
+
+    current_user.favorite_movies.append(movie)
+
+    try:
+        await db.commit()
+    except IntegrityError:
+        await db.rollback()
+        raise HTTPException(
+            status_code=500,
+            detail="An error occurred while adding the movie to favorites."
+        )
+
+    return MessageResponseSchema(message=f"{movie.name} added to favorites")
+
+
