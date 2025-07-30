db2ac12f7302f89946bd15bc7aec529a16221f81
Timur Khvat <timurkhvat@gmail.com>
2025-07-30 08:19:56 +0200

    Add `require_moderator_or_admin` dependency to check user group

--- Cinema/config/dependencies.py
+++ Cinema/config/dependencies.py
@@ -95,3 +95,12 @@ async def get_current_user(
         )
 
     return user
+
+
+def require_moderator_or_admin(current_user: User = Depends(get_current_user)) -> User:
+    if current_user.group.name not in {"admin", "moderator"}:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You do not have permission to perform this action.",
+        )
+    return current_user
