318cc68d5017d26f3dd08cab382b698d5285abb9
Timur Khvat <timurkhvat@gmail.com>
2025-08-02 15:25:09 +0200

    Add OrderMovieSchema Pydantic model for simplified movie representation in orders

---
+++ Cinema/schemas/orders.py
@@ -0,0 +1,14 @@
+from decimal import Decimal
+from pydantic import BaseModel
+from typing import List
+from datetime import datetime
+
+
+class OrderMovieSchema(BaseModel):
+    id: int
+    name: str
+
+    model_config = {"from_attributes": True}
+
+
+
