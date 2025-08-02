4e6f8abe0cbc036540fb1c90b9e9a35b05da7beb
Timur Khvat <timurkhvat@gmail.com>
2025-08-02 15:47:14 +0200

    Add AdminPaymentSchema extending PaymentSchema with user_id

---
+++ Cinema/schemas/payments.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from decimal import Decimal
+from pydantic import BaseModel
+from Cinema.models import PaymentStatusEnum
+
+
+class PaymentSchema(BaseModel):
+    id: int
+    amount: Decimal
+    status: PaymentStatusEnum
+    created_at: datetime
+
+    model_config = {"from_attributes": True}
+
+class AdminPaymentSchema(PaymentSchema):
+    user_id: int
+
+    model_config = {"from_attributes": True}
